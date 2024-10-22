f0ea02cd4e26828854ad1cc63ac943fe5aafbe9f
jpedro002 <joaopsilvavolei@gmail.com>
2024-10-22 14:31:49 -0300

    wip

--- src/app/(app)/index.tsx
+++ src/app/(app)/index.tsx
@@ -1,9 +1,10 @@
+import { HomeHeader } from '@/components/HomeHeader'
 import { ScrollView, Text, View } from 'react-native'
 
 export default function HomeScreen() {
 	return (
-		<ScrollView className="flex-1  ">
-			<Text>Hello, NativeWind v4!</Text>
+		<ScrollView className="flex-1 p-4 pt-6  ">
+			<HomeHeader />
 		</ScrollView>
 	)
 }
---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,23 @@
+import { Bell } from 'lucide-react-native'
+import React from 'react'
+import { Text, View } from 'react-native'
+import { Button } from './ui/button'
+
+export const HomeHeader = () => {
+	return (
+		<View className="h-fit flex-row gap-3 items-center ">
+			<View className="size-14 bg-purple-800 rounded-full items-center justify-center ">
+				<Text className="text-white text-2xl font-roboto-regular">JP</Text>
+			</View>
+
+			<View className="flex-1 pl-4">
+				<Text className="text-2xl font-roboto-regular">John Doe</Text>
+				<Text className="text-gray-500">Welcome back, John</Text>
+			</View>
+
+			<Button variant="link">
+				<Bell size={16} color={'#6b21a8'} fill={'#6b21a8'} />
+			</Button>
+		</View>
+	)
+}
